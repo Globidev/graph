793664474962942f8b60a1dda4493551bc72db0e
Guillaume Depardon <guillaume.depardon@gmail.com>
2016-07-07 01:55:56 +0200

    Added a custom locale for separating thousands on numbers

--- app/srcs/main.cpp
+++ app/srcs/main.cpp
@@ -1,9 +1,12 @@
 #include "application.hpp"
 
+#include "tools/locale.hpp"
+
 int main(int argc, char **argv) {
+    // Makes number more readable on stdout
+    std::cout.imbue(locale::group_numbers_locale);
 
     Application app { argc, argv };
-
     app.run();
 
     return 0;
